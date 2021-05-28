f53f5150859846367dd94f4223e685033fa75831
TeegeeKarzam <83713196+TeegeeKarzam@users.noreply.github.com>
2021-05-28 16:36:15 +0200

    Add files via upload

---
+++ Photo page/accueil/photo.js
@@ -0,0 +1,20 @@
+$('.container').fsScroll()
+
+
+$('.container').fsScroll({
+    loop:true,
+    timing:'smooth',
+duration:1000,
+index:1,
+selectors: {
+    
+        sections:'.sections',
+    
+        section:'.section',
+    
+        page:'.page',
+    
+        active:'.active'
+    
+      }}
+    )
